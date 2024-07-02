0756167d6aca4e7a8334c8417b779514cb86f05e
TrendBurada11 <92115905+trendburada11@users.noreply.github.com>
2024-07-02 19:41:47 +0300

    Update urunsea.js

--- tool/js/urunsea.js
+++ tool/js/urunsea.js
@@ -4,7 +4,7 @@ document.getElementById('btn').addEventListener('click', function() {
   const inputValue = inputField.value.trim();
   const alertDiv = document.getElementById('alert');
 
-  if (inputValue === '8917') {
+  if (inputValue === '8919') {
     window.open('https://www.n11.com/urun/rampage-ally-k11-12-isik-modlu-red-switch-mekanik-antighosting-bilek-destekli-oyuncu-klavye-44056679?magaza=7-24siparis', '_blank');
   } else if (inputValue === '5258') {
     window.open('https://ty.gl/zjugdk5x98', '_blank');
